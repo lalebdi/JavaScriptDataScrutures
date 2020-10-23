e22f7f68f52655272a9d861d78b437aa18d2c3bb
lalebdi <loai.alebdi@gmail.com>
2020-10-23 09:15:24 -0400

    init

---
+++ firstRecurringCharacter.js
@@ -0,0 +1,13 @@
+/* 
+Given an array, return the first recurring character
+
+arr = [2,5,1,2,3,5,1,2,4] 
+returns 2
+
+arr = [2,1,1,2,3,5,1,2,4]
+returns 1
+
+arr = [2,3,4,5]
+returns undefined 
+*/
+
