05d357113223fe15cf0287e8b5844689309f59c3
lalebdi <loai.alebdi@gmail.com>
2020-10-23 09:57:24 -0400

    Done 😇

--- firstRecurringCharacter.js
+++ firstRecurringCharacter.js
@@ -11,3 +11,19 @@ arr = [2,3,4,5]
 returns undefined 
 */
 
+function firstRecurringCharacter2 (input){
+    let map = {}
+    for(let i = 0; i < input.length; i++){
+        if(map[input[i]] !== undefined) {
+            return input[i]
+        } else {
+            map[input[i]] = i
+        }
+        console.log(map)
+    }
+    return undefined
+}
+
+//  O(n) but increased the space complexity 
+
+firstRecurringCharacter2([2,5,1,2,3,5,1,2,4] )
