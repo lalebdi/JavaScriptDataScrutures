f16d5a344a16a22cf3edd774df4ad09320e5345e
lalebdi <loai.alebdi@gmail.com>
2020-10-26 11:47:58 -0400

    Included the drawing

--- graphCode.js
+++ graphCode.js
@@ -1,3 +1,19 @@
+// Creating undriected, unweighted using adjacency list using a hash table. 
+
+/*
+
+            3 --------------- 4 ------------------- 5
+            |                 |                     |
+            |                 |                     |
+            |                 |                     |
+            1 --------------- 2                     6
+            \                 /
+             \               /
+              \             /
+               \--- 0 -----/
+
+*/
+
 class Graph {
     constructor(){
         this.numberOfNodes = 0;
@@ -28,3 +44,11 @@ class Graph {
     }
 }
 
+const myGraph = new Graph();
+myGraph.addVertex('0');
+myGraph.addVertex('1');
+myGraph.addVertex('2');
+myGraph.addVertex('3');
+myGraph.addVertex('4');
+myGraph.addVertex('5');
+myGraph.addVertex('6');
