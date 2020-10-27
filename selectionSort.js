895cb6f4683b08ee5f8c7a175110e972b35375a4
lalebdi <loai.alebdi@gmail.com>
2020-10-27 10:55:20 -0400

    done

--- selectionSort.js
+++ selectionSort.js
@@ -2,7 +2,21 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 
 function selectionSort(array){
-
+    const length = array.length;
+    for ( let i = 0 ; i < length; i++){
+        // set the current index as minimum
+        let min = i;
+        let temp = array[i];
+        for ( let j = i+1; j < length; j++){
+            if (array[j] < array[min]){
+                // update the minimum if current is lower that what we had previously.
+                min = j;
+            }
+        }
+        array[i] = array[min];
+        array[min] = temp;
+    }
+    return array;
 }
 
 selectionSort(numbers);
