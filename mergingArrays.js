9265ece2aef420d301d7b93540b7005ec3c8038b
lalebdi <loai.alebdi@gmail.com>
2020-10-27 10:10:25 -0400

    done

--- mergingArrays.js
+++ mergingArrays.js
@@ -2,7 +2,9 @@
 
 function mergeArrays(arr1, arr2){
     merged = [...arr1, ...arr2]
-    console.log(merged.sort())
+    console.log(merged.sort(function (a, b){
+        return a- b;
+    }))
 }
 
 mergeArrays([0, 3, 4, 31], [4, 6, 30])
