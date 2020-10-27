ce7bf0e212952cddc4c605662adfea3244b01c8a
lalebdi <loai.alebdi@gmail.com>
2020-10-27 15:34:32 -0400

    init

---
+++ quickSort.js
@@ -0,0 +1,9 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function quickSort(array, left, right){
+
+}
+
+// select first, and last index as 2nd and 3rd parameters.
+quickSort(numbers, 0, numbers.length -1);
+console.log(numbers);
