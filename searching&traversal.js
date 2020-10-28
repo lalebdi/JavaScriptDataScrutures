55115ae4826e6afa29e89e0e652a3762c01ee4c7
lalebdi <loai.alebdi@gmail.com>
2020-10-28 09:21:15 -0400

    init

---
+++ searching&traversal.js
@@ -0,0 +1,7 @@
+/*
+main types:
+1- Linear Search or Sequential Search: is a mehtod of finding a target value within a list. It sequentially checks each element in the list.
+2- Binary Search:
+3- Depth First Search:
+4- Breadth First Search:
+*/
