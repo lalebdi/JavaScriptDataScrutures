ec85238a4dbf026334e33d26501459f01241eafe
lalebdi <loai.alebdi@gmail.com>
2020-10-27 20:42:40 -0400

    done

--- sorting.js
+++ sorting.js
@@ -22,8 +22,6 @@
 // 4- Merge: it is good because of divide & conqure. It uses space O(n).
 // 5- Quick: most popular but if you don't pick the pivot properly, you will have a very slow sort. 
 
-
-
 /*
 A sorting algorithm is said to be stable if two objects with equal keys appear in the same order in sorted output as they appear in the input array to be sorted. Some sorting algorithms are stable by nature like Insertion sort, Merge Sort, Bubble Sort, etc. And some sorting algorithms are not, like Heap Sort, Quick Sort, etc.
 */
