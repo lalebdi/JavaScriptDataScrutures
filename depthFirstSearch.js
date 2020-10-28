0b7f4f6deecaacdc3e90a0a7682cb2cf2dcee142
lalebdi <loai.alebdi@gmail.com>
2020-10-28 11:56:10 -0400

    init

---
+++ depthFirstSearch.js
@@ -0,0 +1,7 @@
+//       9
+//   4       20
+//  1   6   15  170
+
+// InOrder = [1, 4, 6, 9, 15, 20, 170]
+// PreOrder = [9, 4, 1, 6, 20, 15, 170] -> starts from the root node and is great for recreating the tree.
+// PostOrder = [1, 6, 4, 15, 170, 20 , 9] -> all the way down then to the top. the children come before the parent. 
