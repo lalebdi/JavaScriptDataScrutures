78de85e4fc64e7a6fd6b9ab514f21577126539d4
lalebdi <loai.alebdi@gmail.com>
2020-10-27 20:41:59 -0400

    tiny addition

--- sorting.js
+++ sorting.js
@@ -27,8 +27,6 @@
 
 
 
-
-
 /*
 A sorting algorithm is said to be stable if two objects with equal keys appear in the same order in sorted output as they appear in the input array to be sorted. Some sorting algorithms are stable by nature like Insertion sort, Merge Sort, Bubble Sort, etc. And some sorting algorithms are not, like Heap Sort, Quick Sort, etc.
 */
