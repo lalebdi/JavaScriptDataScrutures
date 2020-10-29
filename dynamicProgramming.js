08220bf87e8cfa2db31cbe8211d0b46f95ef7b8e
lalebdi <loai.alebdi@gmail.com>
2020-10-28 20:13:28 -0400

    added notes

--- dynamicProgramming.js
+++ dynamicProgramming.js
@@ -1,2 +1,23 @@
 // is a way to slove problems by breaking them down into a collection of subproblems and store their solutions. 
+// Cashing is a way to store values so you can usethem later on. 
 
+// memoization is a special kind of cacheing that involves caching the return value based on its params. If the params don't change then its memoized. So you don't have to calcuate again. 
+
+// Use closures
+function memoizedAddTo80 (){
+    let cache = {};
+    return function(n){
+    if (n in cache){
+        return cache[n];
+    } else {
+        console.log('Time');
+        cache[n] = n + 80;
+        return cache[n];
+    }
+}
+}
+
+const memoized = memoizedAddTo80()
+
+console.log('1: ', memoized(5))
+console.log('2: ', memoized(5))
